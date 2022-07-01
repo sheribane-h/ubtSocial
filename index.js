a2606e833e6dfd8bf65523355eb76080bf6f99ab
sheribane-h <sh47867@ubt-uni.net>
2022-07-01 19:35:34 +0200

    theme customization

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const messageSearch = document.querySelector('#message-search');
 //THEME
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
-
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 
 
@@ -88,3 +93,135 @@ messagesNotification.addEventListener('click', () => {
  
 
  theme.addEventListener('click' , opneThemeModal);
+
+ //FONTS
+
+ //remove active class for spans
+
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+
+
+ fontSizes.forEach(size => {
+    
+    size.addEventListener('click' , () =>{
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle('active');
+        
+    if(size.classList.contains('font-size-1')){
+        fontSize = '10px';
+        root.style.setProperty('--sticky-top-left','5.4rem');
+        root.style.setProperty('--sticky-top-right','5.4rem');
+    }else if(size.classList.contains('font-size-2')){
+        fontSize = '13px';
+        root.style.setProperty('--sticky-top-left','5.4rem');
+        root.style.setProperty('--sticky-top-right','-7rem');
+    }else if(size.classList.contains('font-size-3')){
+        fontSize = '16px';
+        root.style.setProperty('--sticky-top-left','-2rem');
+        root.style.setProperty('--sticky-top-right','-17rem');
+    }else if(size.classList.contains('font-size-4')){
+        fontSize = '19px';
+        root.style.setProperty('--sticky-top-left','-5rem');
+        root.style.setProperty('--sticky-top-right','-25rem');
+    }else if(size.classList.contains('font-size-5')){
+        fontSize = '22px';
+        root.style.setProperty('--sticky-top-left','-10rem');
+        root.style.setProperty('--sticky-top-right','-33rem');
+    }
+
+       //change font size of the root html element
+       document.querySelector('html').style.fontSize = fontSize;
+    })
+
+ 
+ })
+
+
+ //COLORS
+
+ //remove active class from colors
+ const changeActiveColorClass = () =>{
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+ }
+
+ colorPalette.forEach(color => {
+    color.addEventListener('click',() =>{
+        let primary;
+        changeActiveColorClass();
+        if(color.classList.contains('color-1')){
+            primaryHue=252;
+        }else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    })
+ })
+ 
+ //BACKGROUND
+
+ let lightColorLightness;
+ let whiteColorLightness;
+ let darkColorLightness;
+
+ //changes background color
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness' , lightColorLightness);
+    root.style.setProperty('--white-color-lightness' , whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness' , darkColorLightness);
+}
+
+Bg1.addEventListener('click', () =>{
+    //add active class
+    Bg1.classList.add('active');
+    //remove active class from others
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    //remove customized changes from local storage
+    window.location.reload();
+});
+
+
+
+ Bg2.addEventListener('click' , () =>{
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    //add active class
+
+    Bg2.classList.add('active');
+    //remove active class from others
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
+ })
+
+ Bg3.addEventListener('click' , () =>{
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    //add active class
+
+    Bg2.classList.add('active');
+    //remove active class from others
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
+ })
