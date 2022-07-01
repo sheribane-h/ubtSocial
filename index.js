906986abb807e4e5fed41a7dc2261f30b9147ac6
sheribane-h <sh47867@ubt-uni.net>
2022-07-01 16:42:46 +0200

    adding functions

---
+++ index.js
@@ -0,0 +1,90 @@
+//SIDEBAR
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+//MESSAGES
+const messagesNotification = document.querySelector('#message-notification');
+const messages = document.querySelector('.messages');
+const message=messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+//THEME
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+
+
+
+
+
+//SIDEBAR
+//remove active class from all menu items
+
+const changeActiveItem =() =>{
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click' , () =>{
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications'){
+            document.querySelector('.notifications-popup').style.display = 'none';
+
+        }else{
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+//MESSAGES
+//searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            user.style.display = 'flex';
+        }else{
+            user.style.display = 'none';
+        }
+    })
+}
+
+//search chat
+messageSearch.addEventListener('keyup',searchMessage);
+
+
+//highlight messages card when menu item is clicked
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = ' 0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display="none";
+    setTimeout(() =>{
+        messages.style.boxShadow = 'none';
+
+    },2000);
+})
+
+
+
+//THEME CUSTOMIZATION
+
+//open modal
+ const opneThemeModal = () =>{
+    themeModal.style.display = 'grid';
+ }
+
+ //closes modal
+ const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display='none';
+    }
+ }
+
+ //close modal
+ themeModal.addEventListener('click' , closeThemeModal);
+ 
+
+ theme.addEventListener('click' , opneThemeModal);
